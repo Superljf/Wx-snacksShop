04d1177c4374a3cfc1cbe69e965d5ae28eb2e241
Superljf <317558348@qq.com>
2019-09-23 16:48:56 +0800

    finish user layout

--- page/component/user/user.js
+++ page/component/user/user.js
@@ -5,15 +5,37 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+        thumb:'',
+        nickname: '',
+        orders:[],
+        hasAddress: false,
+        address:{}
   },
-
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+        var self = this;
 
+        wx.getUserInfo({
+          success: function (res) {
+            self.setData({
+              thumb: res.userInfo.avataUrl,
+              nickname: res.userInfo.nickname
+            })
+          }
+        }),
+// 发起请求获取订单列表信息
+        wx.request({
+          url: '',
+          success(res) {
+            self.setData({
+              orders: res.data
+            })
+          }
+        })
   },
+
 
   /**
    * 生命周期函数--监听页面初次渲染完成
@@ -26,7 +48,37 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+        var self = this;
+          // 获取本地缓存 地址信息
+          wx.getStorage({
+            key: 'address',
+            success: function(res) {
+              self.setData({
+                hasAddress: true,
+                address: res.data
+              })
+            }
+          })
+  },
+  // 发起支付请求
+  payOrders() {
+        wx.requestPayment({
+          timeStamp: 'String1',
+          nonceStr: 'String2',
+          package: 'String3',
+          singType: 'MD5',
+          paySign: 'String4',
+          success: function(res) {
+              console.log(res)
+          },
+          fail: function(res) {
+            wx.showModal({
+              title: '支付提示',
+              content:'<text>',
+              showCancle: false
+            })
+          }
+        })
   },
 
   /**
