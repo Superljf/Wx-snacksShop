09d6c5ffcbc67cc0fe97cc99748402add168e5ae
Superljf <317558348@qq.com>
2019-09-23 09:31:59 +0800

    Initial Commit

---
+++ app.js
@@ -0,0 +1,14 @@
+App({
+  onLaunch: function () {
+    console.log('App Launch')
+  },
+  onShow: function () {
+    console.log('App show')
+  },
+  onHide: function () {
+    console.log('App Hide')
+  },
+  globalData: {
+    hasLogin: false
+  }
+})
