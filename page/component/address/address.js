59386d03d9e6cf8a01e65dbd4ee6b8734714cdd1
Superljf <317558348@qq.com>
2019-09-24 10:56:46 +0800

    finish address and user function

--- page/component/address/address.js
+++ page/component/address/address.js
@@ -1,41 +1,31 @@
-// page/component/address/address.js
+// page/component/new-pages/user/address/address.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-        address: {
-          name: '',
-          phone: '',
-          detail: ''
-        }
+    address: {
+      name: '',
+      phone: '',
+      detail: ''
+    }
   },
+  onLoad() {
+    var self = this;
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-        var self = this;
-        
-       wx.getStorage({
-          key: 'address',
-          success: (result)=>{
-            self.setData({
-              address : result.data
-            })
-          },
-          fail: ()=>{},
-          complete: ()=>{}
-        });
+    wx.getStorage({
+      key: 'address',
+      success: function (res) {
+        self.setData({
+          address: res.data
+        })
+      }
+    })
   },
   formSubmit(e) {
     const value = e.detail.value;
     if (value.name && value.phone && value.detail) {
       wx.setStorage({
         key: 'address',
-        data: 'value',
-        success () {
+        data: value,
+        success() {
           wx.navigateBack();
         }
       })
@@ -46,54 +36,5 @@ Page({
         showCancel: false
       })
     }
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
   }
 })
--- page/component/user/user.js
+++ page/component/user/user.js
@@ -1,119 +1,73 @@
-// page/component/user/user.js
+// page/component/new-pages/user/user.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-        thumb:'',
-        nickname:'',
-        orders:[],
-        hasAddress: false,
-        address:{}
+  data:{
+    thumb:'',
+    nickname:'',
+    orders:[],
+    hasAddress:false,
+    address:{}
   },
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-        var self = this;
-
-        wx.getUserInfo({
-          success: function (res) {
-            self.setData({
-              thumb: res.userInfo.avataUrl,
-              nickname: res.userInfo.nickName
-            })
-          }
-        }),
-// 发起请求获取订单列表信息
-        wx.request({
-          url: '',
-          success(res) {
-            self.setData({
-              orders: res.data
-            })
-          }
+  onLoad(){
+    var self = this;
+    /**
+     * 获取用户信息
+     */
+    wx.getUserInfo({
+      success: function(res){
+        self.setData({
+          thumb: res.userInfo.avatarUrl,
+          nickname: res.userInfo.nickName
         })
-  },
+      }
+    }),
 
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-        var self = this;
-          // 获取本地缓存 地址信息
-          wx.getStorage({
-            key: 'address',
-            success: function(res) {
-              self.setData({
-                hasAddress: true,
-                address: res.data
-              })
-            }
-          })
-  },
-
-  // 发起支付请求
-  payOrders() {
-        wx.requestPayment({
-          timeStamp: 'String1',
-          nonceStr: 'String2',
-          package: 'String3',
-          singType: 'MD5',
-          paySign: 'String4',
-          success: function(res) {
-              console.log(res)
-          },
-          fail: function(res) {
-            wx.showModal({
-              title: '支付提示',
-              content:'<text>',
-              showCancle: false
-            })
-          }
+    /**
+     * 发起请求获取订单列表信息
+     */
+    wx.request({
+      url: 'http://www.gdfengshuo.com/api/wx/orders.txt',
+      success(res){
+        self.setData({
+          orders: res.data
         })
+      }
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
+  onShow(){
+    var self = this;
+    /**
+     * 获取本地缓存 地址信息
+     */
+    wx.getStorage({
+      key: 'address',
+      success: function(res){
+        self.setData({
+          hasAddress: true,
+          address: res.data
+        })
+      }
+    })
   },
-
   /**
-   * 生命周期函数--监听页面卸载
+   * 发起支付请求
    */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  payOrders(){
+    wx.requestPayment({
+      timeStamp: 'String1',
+      nonceStr: 'String2',
+      package: 'String3',
+      signType: 'MD5',
+      paySign: 'String4',
+      success: function(res){
+        console.log(res)
+      },
+      fail: function(res) {
+        wx.showModal({
+          title:'支付提示',
+          content:'<text>',
+          showCancel: false
+        })
+      }
+    })
   }
 })
